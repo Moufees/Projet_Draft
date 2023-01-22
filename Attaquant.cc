1d69ae33a01f6f3fa77d1e3a3b8cb76cc129e160
Denn68 <91024540+Denn68@users.noreply.github.com>
2023-01-22 14:20:13 +0100

    Add files via upload

--- Attaquant.cc
+++ Attaquant.cc
@@ -2,17 +2,30 @@
 #include "Gardien.hh"
 #include "Milieu.hh"
 #include "Attaquant.hh"
+#include <random>
+#include <fstream>
+#include <iostream>
+
+std::default_random_engine re;
+std::uniform_int_distribution<int> distrib{0, 9};
 
 int Attaquant::dribbler(Joueur& adversaire){
 
     int dribble = this->getDribble() + this->getVitesse() + this->getPhysique();
-
+    int aleatoire;
+    int choix;
+    std::ifstream a("aleatoire.txt");
+    a>>aleatoire;
+    for(int i=0; i<=aleatoire; i++){
+      choix=distrib(re);
+    }
     if (dynamic_cast<Defenseur*>(&adversaire) != nullptr) {
       Defenseur* d = dynamic_cast<Defenseur*>(&adversaire);
       int defense = d->getDefense() + d->getVitesse() + d->getPhysique();
       int diff = dribble - defense;
+      //std::cout<<choix<< std::endl;
       if(diff > 0){
-        if (rand()%10 > 0){
+        if (choix> 0){
           return 1;
         }
         else {
@@ -20,7 +33,7 @@ int Attaquant::dribbler(Joueur& adversaire){
         }
       }
       else if(diff == 0){
-        if (rand()%10 >= 5){
+        if (choix >= 5){
           return 1;
         }
         else {
@@ -28,7 +41,7 @@ int Attaquant::dribbler(Joueur& adversaire){
         }
       }
       else{
-        if (rand()%10 == 9){
+        if (choix == 9){
           return 1;
         }
         else {
@@ -41,7 +54,7 @@ int Attaquant::dribbler(Joueur& adversaire){
       int sortie = g->getSortie();
       int diff = dribble - sortie;
       if(diff > 0){
-        if (rand()%10 > 0){
+        if (choix > 0){
           return 1;
         }
         else {
@@ -49,7 +62,7 @@ int Attaquant::dribbler(Joueur& adversaire){
         }
       }
       else if(diff == 0){
-        if (rand()%10 >= 5){
+        if (choix >= 5){
           return 1;
         }
         else {
@@ -57,7 +70,7 @@ int Attaquant::dribbler(Joueur& adversaire){
         }
       }
       else{
-        if (rand()%10 == 9){
+        if (choix == 9){
           return 1;
         }
         else {
@@ -73,7 +86,7 @@ int Attaquant::tirer(Gardien& gardien){
     int tir = this->getTir();
     int diff = tir - arret;
     if(diff > 0){
-        if (rand()%10 > 0){
+        if (distrib(re) > 0){
             return 1;
         }
         else {
@@ -81,7 +94,7 @@ int Attaquant::tirer(Gardien& gardien){
         }
     }
     else if(diff == 0){
-        if (rand()%10 >= 5){
+        if (distrib(re) >= 5){
             return 1;
         }
         else {
@@ -89,7 +102,7 @@ int Attaquant::tirer(Gardien& gardien){
         }
     }
     else{
-          if (rand()%10 == 9){
+          if (distrib(re)== 9){
             return 1;
         }
         else {
--- test.cc
+++ test.cc
@@ -2,19 +2,34 @@
 #include "Milieu.hh"
 #include "Gardien.hh"
 #include "Jeu.hh"
+#include <fstream>
+#include <iostream>
 
 int main()
 {
+    int aleatoire;
+    std::ifstream a("aleatoire.txt");
+    a>>aleatoire;
+    aleatoire=aleatoire+1;
+    std::ofstream b("aleatoire.txt");
+    b<<aleatoire<< std::endl;
     std::ifstream f("basedejoueurs.csv");
     Jeu cat(f);
-    Gardien g("Yassine Bounou", 99,99, 99);
-    Milieu m1("Sofyan Amrabat", 5, 85, 80, 93, 80, 75, 99);
-    Milieu m2("Neymar Jr", 5, 93, 85, 65, 99, 93, 65);
-    Attaquant a1("Sofiane Bouffal", 1, 75, 82, 80, 95, 85);
-    Attaquant a2("Kylian Mbappe", 1, 80, 99, 80, 91, 90);
-    Attaquant a3("Hakim Ziyech", 3, 97, 85, 82, 90, 90);
-    Defenseur d("Virgil Van Dijk", 8, 88, 65, 99, 88);
-    /*std::cout << a1.poste() << std::endl;
+    Gardien g("Yassine Bounou", 1,1,1);
+    Milieu m1("Sofyan Amrabat", 5,1,1,1,1,1,1);
+    Milieu m2("Neymar Jr", 5, 1,1,1,1,1,1);
+    Attaquant a1("Sofiane Bouffal", 1, 1,1,1,1,1);
+    Attaquant a2("Kylian Mbappe", 1, 1,1,1,1,1);
+    Attaquant a3("Hakim Ziyech", 3, 1,1,1,1,1);
+    Defenseur d("Virgil Van Dijk", 8, 1,1,1,1);
+    a1.dribbler(d);
+    a1.dribbler(d);
+    a1.dribbler(d);
+    a1.dribbler(d);
+    a1.dribbler(d);
+    a1.dribbler(d);
+    a1.dribbler(d);
+    std::cout << a1.poste() << std::endl;
     std::cout << g.poste() << std::endl;
     std::cout << m1.poste() << std::endl;
     std::cout << m2.poste() << std::endl;
@@ -28,7 +43,7 @@ int main()
     std::cout << a2.tirer(g) << std::endl;
     std::cout << a3.tirer(g) << std::endl;
     std::cout << a1.dribbler(static_cast<Defenseur&>(m1)) << std::endl;
-    std::cout << m2.dribbler(static_cast<Defenseur&>(m1)) << std::endl;*/
+    std::cout << m2.dribbler(static_cast<Defenseur&>(m1)) << std::endl;
 
     //Afficher tous les attaquants
     std::cout << "Attaquants :" << std::endl;
