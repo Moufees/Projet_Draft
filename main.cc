e75aded557db8b6be864cbddb673ebeb4feffec1
Mohamed MDB <moufeesmohamed@gmail.com>
2023-01-27 23:33:04 +0100

    Update main.cc

--- main.cc
+++ main.cc
@@ -16,3 +16,19 @@ int main()
     
     return 0;
 }
+
+TEST_CASE("1: Web")
+{
+  CoursWeb css("Learn W3.CSS",50.0,0.89, 9777);
+  css.setTitre("Learn W3.CSS - Part 1");
+  CHECK(css.getTitre() == "Learn W3.CSS - Part 1" );
+  CHECK(css.getId() == 0);
+  CHECK(css.getPrix() == 50.0);
+  CHECK(css.getNote() == 0.89f);
+  css.vote(1);
+  CHECK(css.getAvis() == 9778);
+  css.setPrix(55.5);
+  std::stringstream ss;
+  ss << css;
+  CHECK(ss.str()=="Learn W3.CSS - Part 1,55.500000,0.890011,9778, WEB, -1,-1");
+}
